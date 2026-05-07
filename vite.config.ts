e6e720d6c9f38c3b0223396f9a2d05608e40b86d
Iker <ceballosiker@gmail.com>
2026-05-07 17:03:50 +0000

    chore: configure Playwright with Chromium project

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
+import { configDefaults } from 'vitest/config';
 
 export default defineConfig({
   test: {
     environment: 'happy-dom',
+    exclude: [...configDefaults.exclude, 'e2e/**'],
   },
 });
