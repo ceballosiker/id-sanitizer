f8660b3df03d418b6a25ba106c640eae7e9ff243
Iker <ceballosiker@gmail.com>
2026-05-22 21:46:14 +0000

    refactor(e2e): scope file-input selectors to .dropzone

--- e2e/redact.spec.ts
+++ e2e/redact.spec.ts
@@ -3,7 +3,7 @@ import { makeSolidPng } from './fixtures';
 
 async function uploadRedImage(page: Page): Promise<Locator> {
   await page.goto('/');
-  await page.setInputFiles('input[type=file]', {
+  await page.setInputFiles('.dropzone input[type=file]', {
     name: 'red.png',
     mimeType: 'image/png',
     buffer: makeSolidPng(64, 64, [255, 0, 0]),
